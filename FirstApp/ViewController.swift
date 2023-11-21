6348dedee49b2039c295293b225b36aec35fc21e
resatugurulu <resatugur.ulu@gmail.com>
2023-11-22 02:34:44 +0300

    first commit

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import AuthenticationServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ASAuthorizationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let appleSignInButton = ASAuthorizationAppleIDButton(type: .default, style: .black)
+        appleSignInButton.frame = CGRect(x: 100, y: 200, width: 200, height: 50)
+        appleSignInButton.addTarget(self, action: #selector(handleAppleSignInButtonTap), for: .touchUpInside)
+        self.view.addSubview(appleSignInButton)
     }
 
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @objc func handleAppleSignInButtonTap() {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.performRequests()
+    }
+    @IBAction func changeWhenClicked(_ sender: Any) {
+        if imageView.image == UIImage(named: "GMX") {
+            imageView.image = UIImage(named: "STG")
+        } else {
+            imageView.image = UIImage(named: "GMX")
+        }
+    }
 }
 
